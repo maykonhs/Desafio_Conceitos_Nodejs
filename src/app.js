bfec206d974c9cd723a73b3d1b1ef39cc11dd242
Maykon dos Santos <maykon.dos.santos@avanade.com>
2020-05-12 20:04:36 -0300

    project CRUD

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,93 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  let newRepository = {};
+  let hasId = false;
+
+  repositories.map(repository => {
+    if (repository.id === id) {
+      repository.title = title || repository.title;
+      repository.url = url || repository.url;
+      repository.techs = techs || repository.techs;
+      newRepository = repository;
+      hasId = true;
+    }
+  });
+
+  if (!hasId) {
+    return response.status(400).send();
+  }
+
+  return response.json(newRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  let repositoryToRemove = {};
+  let hasId = false;
+
+  repositories.map(repository => {
+    if (repository.id === id) {
+      repositoryToRemove = repository;
+      hasId = true;
+    }
+  });
+
+  if (!hasId) {
+    return response.status(400).send();
+  }
+
+  const index = repositories.indexOf(repositoryToRemove);
+
+  if (index > -1) {
+    repositories.splice(index, 1);
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  let updatedRepository = {};
+  let hasId = false;
+
+  repositories.map(repository => {
+    if (repository.id === id) {
+      repository.likes += 1;
+      updatedRepository = repository;
+      hasId = true;
+    }
+  });
+
+  if (!hasId) {
+    return response.status(400).send();
+  }
+
+  return response.json(updatedRepository);
 });
 
 module.exports = app;
